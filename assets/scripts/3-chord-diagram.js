ee2afbdaf162a3ae604e63ef65c0ac09e3b0d145
Jiechen Wu <jiwuxb@gigl.polymtl.ca>
2019-02-14 12:38:13 -0500

    Finir le chord chart

--- assets/scripts/3-chord-diagram.js
+++ assets/scripts/3-chord-diagram.js
@@ -25,26 +25,51 @@ function createGroups(g, data, layout, arc, color, total, formatPercent) {
      - Tronquer les noms des stations de BIXI qui sont trop longs (Pontiac et Métro Mont-Royal).
      - Afficher un élément "title" lorsqu'un groupe est survolé par la souris.
   */
-  let group = g.append("g")
+  let group = g
     .selectAll("g")
     .data(layout.groups)
     .enter()
     .append("g")
+    .classed("arcGroup",true)
 
-    group.append("path")
+    let t =group.append("path")
+    .attr("id",d => "arc-"+d.index)
     .attr("fill", d => color(d.index))
     .attr("d", arc);
 
+    let innerRadius = arc.innerRadius()()
     group.append("text")
-    .each(d => { d.angle = (d.startAngle + d.endAngle) / 2; })
-    .attr("dy", ".35em")
-    .attr("transform", function(d) {return  `
-      rotate(${(d.angle * 180 / Math.PI - 90)})
-      translate(${d3.select(this).attr("innerRadius") + 26})
-      ${d.angle > Math.PI ? "rotate(180)" : ""}
-    `})
-    .attr("text-anchor", d => d.angle > Math.PI ? "end" : null)
-    .text(d => data.nameByIndex.get(d.index));
+    .attr("fill", "white")
+    .attr("font-size","12px")
+    .attr("dx","0.6em")
+    .attr("dy","1.4em")
+    .append("textPath")
+    .attr("xlink:href",d => "#arc-"+d.index)
+    .style("text-anchor","start") //place the text halfway on the arc
+    .text(d => {
+      let name = data[d.index].name
+      if (name ==  "Pontiac / Gilford")
+      {
+        name = "Pontiac"
+      }
+      else if (name == "Métro Mont-Royal (Rivard/Mont-Royal)") 
+      {
+        name = "Métro Mont-Royal"
+      }
+      return name
+      }
+      );
+
+    group.append("svg:title")
+    .text(d => {
+      let sum = 0
+      data[d.index].destinations.forEach(dd=>
+        {
+          sum+=dd.count
+        }
+      )
+      return data[d.index].name+": "+formatPercent(sum/total)+" des departs"
+    });
 
 }
 
@@ -66,15 +91,21 @@ function createChords(g, data, layout, path, color, total, formatPercent) {
      - Créer les cordes du diagramme avec une opacité de 80%.
      - Afficher un élément "title" lorsqu'une corde est survolée par la souris.
   */
-    g.append("g")
-    .attr("fill-opacity", 0.67)
-    .selectAll("path")
+    let group = g
+    .selectAll("path.chord")
     .data(layout)
     .enter()
-    .append("path")
-    .attr("fill", d => color(d.source.index))
-    .attr("d", path);
+    
 
+    group.append("path")
+    .attr("fill", d => color(d.source.index))
+    .attr("class","chord")
+    .attr("d", path)
+    .append("svg:title")
+    .html((d,i) => {
+      return data[d.source.index].name+" -> "+data[d.target.index].name+" "+formatPercent(d.source.value/total)+" des departs\n"
+      +data[d.target.index].name+" -> "+data[d.source.index].name+" "+formatPercent(d.target.value/total)+" des departs\n"
+    });
 }
 
 /**
@@ -88,5 +119,20 @@ function initializeGroupsHovered(g) {
        opacité de 80%. Toutes les autres cordes doivent être affichées avec une opacité de 10%.
      - Rétablir l'affichage du diagramme par défaut lorsque la souris sort du cercle du diagramme.
   */
+
+g.selectAll(".arcGroup")
+ .on("mouseover",function(ad){
+    let chords = g.selectAll(".chord")
+    chords.each(function(cd)
+    {
+      if (ad.index!=cd.source.index && ad.index!=cd.target.index)
+      {
+        d3.select(this).classed("fade", true);
+      }
+    })
+ })
+ .on("mouseout",function(d){
+  let arcs = g.selectAll(".chord").classed("fade", false)
+ })
 
 }
